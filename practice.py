1dc6b5ccdbda70ee34571e652b5c5fdb2c830eaa
Ibrahim Gurhan <ibrahimgurhan.dev@gmail.com>
2021-03-29 17:09:14 -0400

    new lab

---
+++ practice.py
@@ -0,0 +1,4 @@
+grocery=["cheese","salad","sausage",["choc chip cookie dough ice cream", "mac", "cheetos"],[["root beer","sprite"],["jack daniels", "vodka"]], "sausage",["cheetos", "root beer", "vodka"]]
+
+print(grocery[4][-1])
+
