25d28620597867860437f524bfceef96f93eae43
Ibrahim Gurhan <ibrahimgurhan.dev@gmail.com>
2021-03-29 19:32:27 -0400

    new lab

--- advlist/complex01.py
+++ advlist/complex01.py
@@ -20,3 +20,4 @@ print(list1)
 print(list1[4])
 print(list1[4][0])
 
+
--- practice.py
+++ practice.py
@@ -1,4 +1,28 @@
-grocery=["cheese","salad","sausage",["choc chip cookie dough ice cream", "mac", "cheetos"],[["root beer","sprite"],["jack daniels", "vodka"]], "sausage",["cheetos", "root beer", "vodka"]]
+heroes=  {
+"wolverine":
+    {"real name": "James Howlett",
+    "powers": "regeneration",
+    "archenemy": "Sabertooth",},
+"harry potter":
+    {"real name": "Harry Potter",
+    "powers": "he's a wizard",
+    "archenemy": "Voldemort",},
+"agent fitz":
+    {"real name": "Leopold Fitz",
+    "powers": "intelligence",
+    "archenemy": "Hydra",}
+}
 
-print(grocery[4][-1])
 
+while True:
+    char_name = input("Which character do you want to know about? (wolverine, harry Potter, agent fitz)")
+    if char_name in heroes.keys():
+      break
+    print("Invalid name entered")
+while True:
+    char_stat = input("What statistic do you want to know about? (real name, powers, archenemy)")
+    if char_stat in heroes[char_name].keys():
+      break
+    print("Invalid stat entered")
+
+print(f"{char_name.capitalize()}'s {char_stat} is: {heroes[char_name][char_stat]}")
