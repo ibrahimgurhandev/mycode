b807b6c3fad590b3af936749efc9c07eaa8d9b8c
Ibrahim Gurhan <ibrahimgurhan.dev@gmail.com>
2021-03-31 14:45:57 -0400

    new lab

---
+++ challenges/chal02.py
@@ -0,0 +1,31 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+def animals_from_ne_farm():
+    for animal in farms[0]["agriculture"]:
+        print(animal)
+animals_from_ne_farm()
+
+def choose_farm():
+    farm_chosen = input("what farm will you like to look at: NE Farm, W Farm or SE Farm: ").upper()
+    for item in farms:
+        if farm_chosen in item["name"]:
+            for animal in item["agriculture"]:
+                print(animal)
+choose_farm()
+
+def choose_farm_only_animals():
+    farm_chosen = input("what farm will you like to look at: NE, W or SE: ").upper()
+    for item in farms:
+        if farm_chosen in item["name"]:
+            for animal in item["agriculture"]:
+                if animal not in {"carrots", "celery"}:
+                    print(animal)
+choose_farm_only_animals()
+
+
+
+
+
+
